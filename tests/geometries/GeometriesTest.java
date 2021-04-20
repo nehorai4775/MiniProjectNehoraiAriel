fdf0bc362c68d9ad967cc1170ea51c9625e9d8be
ariel benshushan <benshoshan60@gmail.com>
2021-04-21 00:21:43 +0300

    ya gever

--- tests/geometries/GeometriesTest.java
+++ tests/geometries/GeometriesTest.java
@@ -1,6 +1,12 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,8 +14,16 @@ class GeometriesTest {
 
     @Test
     void findIntsersections() {
-        Geometries list = new Geometries();
-        //BAV
-        assertNull(list,"the list is empty");
+
+        Geometries list = new Geometries(new Triangle(new Point3D(0,1.5,0),new Point3D(1,0,0),new Point3D(-1,0,0)),
+                new Sphere(new Point3D(1, 0, 0),1d),
+                new Plane(new Point3D(1,0,0),new Point3D(0,1,0),new Point3D(0,2,0)));
+
+
+
+assertNull(list.findIntsersections(new Ray(new Point3D(1,1,0),new Vector(0,0,1))));
+
+
+
     }
 }
